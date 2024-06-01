b3e0f1e1ff8c1f38fe1a2268638eef9d87ba6f94
Dhanush <dhanushbommavaram@gmail.com>
2024-06-01 14:13:47 +0530

    gestures, not working

--- app/src/main/java/com/example/arshopping/ArActivity.kt
+++ app/src/main/java/com/example/arshopping/ArActivity.kt
@@ -1,6 +1,8 @@
 package com.example.arshopping
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,14 +14,21 @@ import com.google.ar.sceneform.rendering.Texture
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.AugmentedFaceNode
 import java.util.concurrent.CompletableFuture
+import kotlin.math.abs
 
-class ArActivity : AppCompatActivity() {
+
+class ArActivity : AppCompatActivity(),GestureDetector.OnGestureListener {
     private lateinit var  arFragment: ArFragment
     private var faceRenderable:ModelRenderable?=null
     private var faceTexture:Texture?=null
     private var curIndex:Int=0
 
     private val faceNodeMap = HashMap<AugmentedFace,AugmentedFaceNode>()
+
+
+    private lateinit var gestureDetector: GestureDetector
+    private val swipeThreshold = 1
+    private val swipeVelocityThreshold = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +47,36 @@ class ArActivity : AppCompatActivity() {
                 removeUntrackedFaces()
             }
         }
+
+
+
+        gestureDetector = GestureDetector(this)
+        fragment.view?.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+            true
+        }
+
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    private fun onSwipeRight() {
+        curIndex = (curIndex - 1 + glassesArray.size)% glassesArray.size
+//        loadModel()
+        Toast.makeText(applicationContext,"right",Toast.LENGTH_LONG).show()
+    }
+
+    private fun onSwipeLeft() {
+        curIndex++
+        curIndex%= glassesArray.size
+//        loadModel()
+        Toast.makeText(applicationContext,"right",Toast.LENGTH_LONG).show()
+
     }
 
     private fun addTrackedFaces() {
@@ -85,5 +124,46 @@ class ArActivity : AppCompatActivity() {
                 Toast.makeText(this,"Error loading model: $it",Toast.LENGTH_LONG).show()
                 null
             }
+    }
+
+    override fun onDown(e: MotionEvent): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onShowPress(e: MotionEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLongPress(e: MotionEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            val diffY = e2.y - e1?.y!!
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > swipeThreshold && abs(velocityX) > swipeVelocityThreshold) {
+                    if (diffX > 0) {
+                        Toast.makeText(applicationContext, "Left to Right swipe gesture", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(applicationContext, "Right to Left swipe gesture", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return true
     }
 }
